cbfd1c0c1a66f24d415f64d40472e293225af9e7
Rahul Rajaram <rv.rahul2023@gmail.com>
2022-01-11 18:59:37 -0500

    Changed RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -18,9 +19,29 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  //RR 1/11/2022
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Auto m_auto = new Auto();
+  private final BallIntake m_BallIntake = new BallIntake();
+  private final Climbing m_Climbing = new Climbing();
+  private final Elevator m_Elevator = new Elevator();
+  private final RobotDrive m_RobotDrive = new RobotDrive();
+  private final Shooting m_Shooting = new Shooting();
+  private final ShootingRotate m_ShootingRotate = new ShootingRotate();
+  
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final ExampleCommand m_exampleCommand = new ExampleCommand(m_exampleSubsystem);
+  private final AutoCommand m_AutoCommand = new AutoCommand(m_auto);
+  private final BallIntakeCommand m_BallIntakeCommand = new BallIntakeCommand(m_BallIntake);
+  private final BallShootBottomCommand m_BallShootBottomCommand = new BallShootBottomCommand(m_Shooting);
+  private final BallShootTopCommand m_BallShootTopCommand = new BallShootTopCommand(m_Shooting);
+  private final ClimbingHangCommand m_ClimbingHangCommand = new ClimbingHangCommand(m_Climbing);
+  private final ClimbingTraverseCommand m_ClimbingTraverseCommand = new ClimbingTraverseCommand(m_Climbing);
+  private final DriveCommand m_DriveCommand = new DriveCommand(m_RobotDrive);
+  private final ElevatorMoveBottomCommand m_ElevatorMoveBottomCommand = new ElevatorMoveBottomCommand(m_Elevator);
+  private final ShootingRotateCommand m_ShootingRotateCommand = new ShootingRotateCommand(m_ShootingRotate);
+
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -53,6 +74,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return m_AutoCommand;
   }
 }
