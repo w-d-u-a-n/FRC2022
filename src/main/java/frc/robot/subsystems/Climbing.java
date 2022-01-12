376eb2350229f2ec8d39cc826d194f9f5a3951d8
hchung04 <77026034+hchung04@users.noreply.github.com>
2022-01-12 18:37:41 -0500

    Added import Climbing

--- src/main/java/frc/robot/subsystems/Climbing.java
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climbing extends SubsystemBase {
