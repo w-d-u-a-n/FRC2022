28fb8083021f5cd38e2a0040cd3218ad1d1d90ba
Rahul Rajaram <rv.rahul2023@gmail.com>
2022-01-12 18:41:30 -0500

    drive

--- src/main/java/frc/robot/subsystems/RobotDrive.java
+++ src/main/java/frc/robot/subsystems/RobotDrive.java
@@ -4,11 +4,29 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class RobotDrive extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  private PWMVictorSPX m_rearLeft = new PWMVictorSPX(0); //change
+  private PWMVictorSPX m_frontLeft = new PWMVictorSPX(1);
+  private MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
+
+  private PWMVictorSPX m_rearRight = new PWMVictorSPX(2);
+  private PWMVictorSPX m_frontRight = new PWMVictorSPX(3);
+  private MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
+
+  private DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
   public RobotDrive() {}
+
+  public void arcadeDriveSimple(double y, double x){
+    DifferentialDrive.arcadeDriveIK(y, x, false);
+  }
 
   @Override
   public void periodic() {
