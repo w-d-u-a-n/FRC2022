071dc0354a6cc6263dd2e66c217a7b81d433b3cf
hchung04 <77026034+hchung04@users.noreply.github.com>
2022-01-12 19:00:46 -0500

    Constants edit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -13,5 +16,63 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    //Constants copied and pasted from 2020 code
+    public static final class AutoConstants{
+        public static final XboxController m_driverController = new XboxController(1);
+        public static final Joystick m_driverJoystick = new Joystick(2);
+        
+        /**
+         * TO-DO: Edit all constants to correspond to the correct port - HC 01/12/2022
+         */
+        public static final double kP = 1/3600;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kDistance = 100;
+        public static final double kSpeed = 0.7;
+        public static final int IntakePort = 7;
+        public static final int OuttakePort = 4 ;
+        public static final int shootRotate = 8;
+        public static final int hanging = 6;
+        public static final int elevator = 5;
+        public static final int rearLeftDrive = 0;
+        public static final int frontLeftDrive = 1;
+        public static final int rearRightDrive = 2;
+        public static final int frontRightDrive = 3;
+        /*
+        private SpeedController m_rearLeft = new PWMVictorSPX(0);
+        private SpeedController m_frontLeft = new PWMVictorSPX(1);
+        private SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+      
+        private SpeedController m_rearRight = new PWMVictorSPX(2);
+        private SpeedController m_frontRight = new PWMVictorSPX(3);
+        private SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+      
+        private DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);*/
+
+
+        //BEN DRIVE CONSTANTS
+        public static final double ksVolts=5;
+        public static final double kvVoltSecondsPerMeter=5;
+        public static final double kaVoltSecondsSquaredPerMeter=5;
     
+        public static final double kPDriveVel=5;
+
+        public static final double kTrackwidthMeters = 0.55;
+        public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackwidthMeters);
+
+        public static final double kMaxSpeedMetersPerSecond=5 ;
+        public static final double kMaxAccelerationMetersPerSecondSquared=5;
+
+        // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = 0.7;
+
+        public static final int[] kLeftEncoderPorts= {0,1};
+        public static final int[] kRightEncoderPorts= {2,3};
+        public static final boolean kLeftEncoderReversed = false;
+        public static final boolean kRightEncoderReversed = false;
+
+        public static final double kEncoderDistancePerPulse=5;
+    }
 }
